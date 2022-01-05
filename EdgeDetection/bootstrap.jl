80caf9df0894215f51043d45ddb3f30ed5553708
VladHornai <54432287+VladHornai@users.noreply.github.com>
2022-01-05 21:32:36 +0200

    Delete bootstrap.jl

--- EdgeDetection/bootstrap.jl
+++
@@ -1,5 +0,0 @@
-pwd() == joinpath(@__DIR__, "bin") && cd(@__DIR__) # allow starting app from bin/ dir
-
-using EdgeDetection
-push!(Base.modules_warned_for, Base.PkgId(EdgeDetection))
-EdgeDetection.main()
